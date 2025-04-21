10424f2f10c7318e4359906cdc3bfbe138f61794
Ranveersingh1113 <161949661+Ranveersingh1113@users.noreply.github.com>
2025-04-22 02:57:41 +0530

    Update DatabaseConnector.java

--- src/database/DatabaseConnector.java
+++ src/database/DatabaseConnector.java
@@ -1,1 +1,15 @@
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DatabaseConnector {
+    private static final String URL = "jdbc:mysql://localhost:3306/weather_app";
+    private static final String USER = "root"; // your MySQL username
+    private static final String PASSWORD = "Nidhi*2907"; // your MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+}
